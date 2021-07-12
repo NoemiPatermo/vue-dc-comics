cdfbe0ad117bff81c5a8555de2e3bb87b1fccf25
noemi86-cpu <noemipatermo@gmail.com>
2021-07-12 22:55:25 +0200

    work

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        FOOTER
+    <div class="footer-container ">
+        <div class="footer-login">
+            <button>SIGN-UP NOW!</button>
+        </div>
+        <div class="footer-content">
+            <span>FOLLOW US</span>
+            <div class="footer-icon">
+                <img src="../assets/footer-facebook.png" alt="">
+                <img src="../assets/footer-twitter.png" alt="">
+                <img src="../assets/footer-youtube.png" alt="">
+                <img src="../assets/footer-pinterest.png" alt="">
+                <img src="../assets/footer-periscope.png" alt="">
+            </div>
+            
+        </div>
+        
     </div>
 </template>
 
@@ -9,3 +23,36 @@ export default {
     name:'Footer'
 }
 </script>
+
+<style lang="scss" scoped>
+    .footer-container{
+        height: 80px;
+        background-color: #191919;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+            
+    
+    
+        .footer-login button{
+           margin-top: 10px;
+           color: white;
+           background-color: #191919;
+           width: 100px;
+           padding: 5px;
+           border: 1px solid blue;
+        }
+        .footer-content{
+            
+            
+            span{
+                color: blue;
+                font-size: 15px;
+            }
+            img{
+                padding: 5px;
+            }
+        }
+    }
+
+</style>
